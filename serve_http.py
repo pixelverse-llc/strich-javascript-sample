f4034c6ca32ba5663b1db2fb7f2a29c9867c1e2f
Alex Suzuki <alex@pixelverse.ch>
2024-02-29 14:47:17 +0100

    Adapt python scripts to disable caching when serving the sample

--- serve_http.py
+++ serve_http.py
@@ -2,8 +2,16 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
 import sys
 
+
+class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def send_response_only(self, code, message=None):
+        super().send_response_only(code, message)
+        self.send_header('Cache-Control', 'no-store, must-revalidate')
+        self.send_header('Expires', '0')
+
+
 if __name__ == '__main__':
     # Minimal example of serving the directory contents via HTTPS
-    httpd = HTTPServer(('0.0.0.0', 8888), SimpleHTTPRequestHandler)
-    print(f'Serving directory on http://0.0.0.0:8888 (plain HTTP, you need to use ngrok or similar tool for SSL)')
+    httpd = HTTPServer(('0.0.0.0', 8888), NoCacheHTTPRequestHandler)
+    print(f'Serving directory on http://0.0.0.0:8888 (caching is disabled, plain HTTP, you need to use ngrok or similar tool for SSL)')
     httpd.serve_forever()
--- serve_https.py
+++ serve_https.py
@@ -2,6 +2,14 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
 import sys
 
+
+class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def send_response_only(self, code, message=None):
+        super().send_response_only(code, message)
+        self.send_header('Cache-Control', 'no-store, must-revalidate')
+        self.send_header('Expires', '0')
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
@@ -9,7 +17,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # Minimal example of serving the directory contents via HTTPS
-    httpd = HTTPServer(('0.0.0.0', 4443), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(('0.0.0.0', 4443), NoCacheHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket(httpd.socket, keyfile=sys.argv[1], certfile=sys.argv[2], server_side=True)
-    print('Serving directory on https://0.0.0.0:4443')
+    print('Serving directory on https://0.0.0.0:4443, caching is disabled')
     httpd.serve_forever()
