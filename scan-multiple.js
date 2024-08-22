641700c8c4badfbdd54974c76134c09925e14a84
Alex Suzuki <alex@pixelverse.ch>
2024-08-22 15:42:30 +0200

    chore: updated SDK to 1.5.0

--- scan-multiple.js
+++ scan-multiple.js
@@ -2,7 +2,7 @@
  * Import STRICH SDK via ES6 import clause. It is recommended to pin a specific version, especially for
  * business-critical applications.
  */
-import {StrichSDK, BarcodeReader} from "https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.4.8";
+import {StrichSDK, BarcodeReader} from "https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.5.0";
 
 let theBarcodeReader = null;
 
