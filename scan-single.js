55509e5e480227cfe277a3060f81c90941880e7a
Alex Suzuki <alex@pixelverse.ch>
2024-08-26 15:12:47 +0200

    chore: updated SDK to 1.5.1

--- scan-single.js
+++ scan-single.js
@@ -2,7 +2,7 @@
  * Import STRICH SDK via ES6 import clause. It is recommended to pin a specific version, especially for
  * business-critical applications.
  */
-import {StrichSDK, BarcodeReader} from "https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.5.0";
+import {StrichSDK, BarcodeReader} from "https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.5.1";
 
 /**
  * Store the scanned code in the session storage and return to the home page.
